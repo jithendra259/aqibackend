3a90abb289023bc74d60287e420da871845d3f3e
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-08-10 20:10:10 +0530

    model name update gogle auth update

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def generate():
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.0-flash",
             contents=user_input
         )
         return jsonify({"response": response.text})
